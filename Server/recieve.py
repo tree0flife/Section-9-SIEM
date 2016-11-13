48b751d2d1b6a729b798379ce3d9f0ed3084853e
Debonairesnake6 <canadianboyz@hotmail.ca>
2016-11-13 16:31:52 -0500

    Update recieve.py

--- Server/recieve.py
+++ Server/recieve.py
@@ -18,71 +18,72 @@ except socket.error as e:
 s.listen(100)
 print('Listening for connections')
 
-def threaded_client(conn):
-	conn.send(str.encode("Login Succeeded"))
-	file = open("/home/matrix/testing/Server/tmp/myData.zip", "w+b")
+class methods:
+	def threaded_client(conn):
+		conn.send(str.encode("Login Succeeded"))
+		file = open("/home/matrix/testing/Server/tmp/myData.zip", "w+b")
 
-	while True:
-		try:
+		while True:
+			try:
+				data = conn.recv(1024)
+				file.write (data)
+			except ConnectionResetError:
+				break
+			if not data:
+				break
+		file.close()
+		zipArch = ZipFile ("myData.zip", "r")
+		zipArch.extractall("/home/matrix/testing/Server/clientInfo")
+			conn.close()
+		zipArch.close()
+		authUser = 1
+
+	def authUser(conn):
+		authCheck = open ("/home/matrix/testing/Server/users.txt", "r")
+		counter = 0
+		cont = 0
+
+		while True:
 			data = conn.recv(1024)
-			file.write (data)
-		except ConnectionResetError:
-			break
-		if not data:
-			break
-	file.close()
-	zipArch = ZipFile ("myData.zip", "r")
-	zipArch.extractall("/home/matrix/testing/Server/clientInfo")
-	conn.close()
-	zipArch.close()
-	authUser = 1
+			clientID = data.decode('utf-8')
+			data = conn.recv(1024)
+			clientPWD = data.decode('utf-8')
+			data = conn.recv(1024)
+			clientTOK = data.decode('utf-8')
+			for userInfo in authCheck:
+				if counter == 0:
+					if clientID == userInfo:
+						cont = 1
+						counter = 1
+					else:
+						cont = 0
+						counter = 1
+				elif counter == 1 and cont == 1:
+					if clientPWD == userInfo:
+						cont = 2
+						counter = 2
+					else:
+						cont = 0
+						counter = 2
+				elif counter == 2 and cont == 2:
+					if clientTOK == userInfo:
+						conn.send(str.encode('welcome'))
+						confUser = 1
+						break
+					elif clientTOK == "none":
+						conn.send(str.encode('welcome'))
+						token = random.random(100000, 999999)
+						conn.send(str.encode(token))
+						userInfo.write(token)
+						confUser = 1
+						break
+					else:
+						cont = 0
+						counter = 0
+			if confUser == 0:
+				conn.send(str.encode('Invaild Login')
 
-def authUser(conn):
-	authCheck = open ("/home/matrix/testing/Server/users.txt", "r")
-	counter = 0
-	cont = 0
-
-	while True:
-		data = conn.recv(1024)
-		clientID = data.decode('utf-8')
-		data = conn.recv(1024)
-		clientPWD = data.decode('utf-8')
-		data = conn.recv(1024)
-		clientTOK = data.decode('utf-8')
-		for userInfo in authCheck:
-			if counter == 0:
-				if clientID == userInfo:
-					cont = 1
-					counter = 1
-				else:
-					cont = 0
-					counter = 1
-			elif counter == 1 and cont == 1:
-				if clientPWD == userInfo:
-					cont = 2
-					counter = 2
-				else:
-					cont = 0
-					counter = 2
-			elif counter == 2 and cont == 2:
-				if clientTOK == userInfo:
-					conn.send(str.encode('welcome'))
-					confUser = 1
-					break
-				elif clientTOK == "none":
-					conn.send(str.encode('welcome'))
-					token = random.random(100000, 999999)
-					conn.send(str.encode(token))
-					userInfo.write(token)
-					confUser = 1
-					break
-				else:
-					cont = 0
-					counter = 0
-		if confUser == 0:
-			conn.send(str.encode('Invaild Login')
-
-	threaded_client(conn)
+		self.threaded_client(conn)
 
 while (1):
 
