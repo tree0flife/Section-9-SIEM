9414f0f92c279e41f2ffdb69995aa306974ce9e3
Sylvain <sylvain.hp.lu@gmail.com>
2016-12-11 17:16:26 -0500

    Download button.  FINAL? ------------ Need to fix Logout?

--- Server/siem_site/core/urls.py
+++ Server/siem_site/core/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='core.html'), name='core'),
     url(r'^bash_history_client/$', views.View_Bash_History_Client.as_view(), name='bash_history_client'),
     url(r'^outbound_ip_client/$', views.View_Outbound_IP_Client.as_view(), name='outbound_ip_client'),
-    url(r'^download/$', views.View_Download_Client, name='browser_history_client'),
+    url(r'^download/$', views.View_Download_Client, name='download'),
 ]
